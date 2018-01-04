582b2f1b58be8ea2180a9bcf7586f4fffd3bd937
liuhuayo <995058772@qq.com>
2018-01-04 15:59:55 +0800

    第二次提交

--- src/main/java/com/example/ims/config/WebMvcConfig.java
+++ src/main/java/com/example/ims/config/WebMvcConfig.java
@@ -1,4 +1,11 @@
 package com.example.ims.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig  extends WebMvcConfigurerAdapter{
+
 }
